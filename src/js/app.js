7a446c66983ad8fe5e9d21e605e1c9ac93211484
polara Hiren <hiren.polara22@gmail.com>
2022-03-10 20:13:39 +0530

    landing page modify and athletes page slider design

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,27 @@ try {
 require('slick-carousel/slick/slick');
 
 jquery(document).ready(function () {
+    jquery('.livestrem-slider').slick({
+        dots: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: true,
+                    prevArrow:"<button type='button' class='slick-prev slick-arrow'><img src='images/athletes/prev.png' alt='arrow image' /></button>",
+                    nextArrow:"<button type='button' class='slick-next slick-arrow'><img src='images/athletes/next.png' alt='arrow image' /></button>"
+                }
+            }
+        ]
+    });
+    
+    
     jquery('.fans-features-slider').slick({
         dots: false,
         speed: 300,
