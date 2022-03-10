ee0cd41525e38c6891850295efb77fb3663846d2
polara Hiren <hiren.polara22@gmail.com>
2022-03-10 19:33:07 +0530

    fans page design updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,44 @@
-// require('jquery');
+const jquery = require('jquery');
+
+try {
+    window.$ = window.jQuery = require('jquery');
+} catch (e) {}
+
+// slick js
+require('slick-carousel/slick/slick');
+
+jquery(document).ready(function () {
+    jquery('.fans-features-slider').slick({
+        dots: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        responsive: [{
+                breakpoint: 1800,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 540,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
