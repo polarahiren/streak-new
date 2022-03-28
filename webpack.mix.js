b8179b71b3a74848a5afea43a6f462f2f3c6738f
hiren polara <hiren.polara22@gmail.com>
2022-03-29 01:02:17 +0530

    landing page design change

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'public/js')
     .sass('src/scss/app.scss', 'public/css')
-    .copy('src/fonts', 'mb1-gaming/fonts')
+    .copy('src/fonts', 'public/fonts')
 
     mix.options({
         processCssUrls: false
