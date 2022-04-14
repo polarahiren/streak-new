617c293b968bfa5c2e576b6d3abeef55a325f4df
hiren polara <hiren.polara22@gmail.com>
2022-04-14 19:16:25 +0530

    streak new design athletes page

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'public/js')
     .sass('src/scss/app.scss', 'public/css')
+    .sass('src/scss/app-streak.scss', 'public/css')
     .copy('src/fonts', 'public/fonts')
 
     mix.options({
